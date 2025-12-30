192d97e02da7c2b07512cc65e3958095138a2aea
manish <support@technofra.com>
2025-12-30 17:04:02 +0530

    add favico

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,9 +5,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="Smarthr - Bootstrap Admin Template">
-    <meta name="keywords" content="admin, estimates, bootstrap, business, html5, responsive, Projects">
-    <meta name="author" content="Technofra technologies - Bootstrap Admin Template">
     <meta name="robots" content="noindex, nofollow">
     <title>Texon Admin Panel</title>
 
