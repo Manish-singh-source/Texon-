cb4d93d829d97ef74893490517ef41cdfbddfcce
manish <support@technofra.com>
2026-01-05 10:13:54 +0530

    tension

--- resources/views/layouts/masters.blade.php
+++ resources/views/layouts/masters.blade.php
@@ -3542,7 +3542,7 @@
 			});
 		});
 	</script>
-
+@yield('scripts')
 <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"version":"2024.11.0","token":"d05194593ce14c8fa5c20a9737ff5d07","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
 </body>
 </html>
